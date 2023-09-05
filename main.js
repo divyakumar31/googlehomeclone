bdbce0d3ba41fb8a114ed2e1499e336ce102baec
divyakumar patel <d.b.patel691@gmail.com>
2023-09-05 12:12:20 +0530

    dark mode button added

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+const darkmode = document.querySelector('#setting');
+const settingClick = document.querySelector('.setting-div');
+const buttonShow = document.querySelector('.buttonShow');
+const darkmodeswitch = document.querySelector('.dark-mode-buttons');
 
 if (prefersColorScheme.matches) {
     document.body.classList.toggle('dark-theme-colors');
+    darkmode.querySelector('button:nth-child(1)').classList.toggle('active');
+    darkmode.querySelector('button:nth-child(2)').classList.toggle('active');
 }
+
+darkmodeswitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-colors');
+    darkmode.querySelector('button:nth-child(1)').classList.toggle('active');
+    darkmode.querySelector('button:nth-child(2)').classList.toggle('active');
+});
+
+settingClick.addEventListener('click', () => {
+    if (buttonShow.style.display === "none") {
+        buttonShow.style.display = "block";
+    } 
+    else {
+        buttonShow.style.display = "none";
+    }
+});
